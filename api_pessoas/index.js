46a96fff3b6d9cca15faae65a7d7d62230ed90be
Matheus <matheusdemoura0408@gmail.com>
2025-08-27 22:17:22 -0300

    api pessoas

--- api_pessoas/index.js
+++ api_pessoas/index.js
@@ -1,1 +1,12 @@
-console.log("Teste")
+const express = require("express");
+
+const app = express();
+
+
+const pessoas = [];
+
+
+app.listen(8080, ()=>{
+    console.log("O servidor esta rodando na porta 8080");
+})
+

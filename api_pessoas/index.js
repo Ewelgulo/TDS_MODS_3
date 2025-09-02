e85f986ba1b3ab2c75e20e777982e395bf0a2910
Matheus <matheusdemoura0408@gmail.com>
2025-09-01 22:20:57 -0300

    api pessoas

--- api_pessoas/index.js
+++ api_pessoas/index.js
@@ -2,11 +2,46 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
 
-const pessoas = [];
+const pessoas = [
+    { Nome: "Matheus", telefone: "46 40028922" },
+    { Nome: "Marcao Ruim", telefone: "46 52429983" }
+];
+console.log("Batman, eu capturei um Pokemon");
+
+app.get("/pessoas", (request, response) => {
+    return response.send(pessoas);
+});
+
+app.get("/pessoas/:nome", (request, response)=>{
+    const nome = request.params.nome;
+
+    const pessoa = pessoas.filter(i => i.Nome === nome);
+    
+    if(pessoa.length<=0){
+        return response.send({mensagem: "Nenhuma pessoa encontrada com esse nome!"});
+    }
+    return response.send(pessoa);
+});
 
 
-app.listen(8080, ()=>{
+app.post("/pessoas", (request, response) => {
+    const nome = request.body.nome;
+    const telefone = request.body.telefone;
+    const status = request.body.status;
+
+    pessoas.push({
+        nome,
+        telefone,
+        status
+    });
+
+    return response.send(pessoas);
+});
+
+
+app.listen(8080, () => {
     console.log("O servidor esta rodando na porta 8080");
-})
+});
 
